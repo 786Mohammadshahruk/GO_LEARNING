bfa84bcad24aa265b8ee0817944b5608298872d6
Mohammad Shahruk <mshahruk57@gmail.com>
2024-09-30 16:26:48 +0530

    string function

--- strings/strings.go
+++ strings/strings.go
@@ -9,4 +9,15 @@ func main() {
 	var data string = "apple,orange,banana"
 	var strArray []string = strings.Split(data, ",")
 	fmt.Println(strArray)
+
+	str := "    Hello Let's Go          "
+	fmt.Println(str)
+	trimmed := strings.TrimSpace(str)
+	fmt.Println(trimmed)
+
+	str1 := "Mohammad"
+	str2 := "Shahruk"
+	result := strings.Join([]string{str1, str2}, ",")
+	fmt.Println(result)
+
 }
